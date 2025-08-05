b5b1c4aa43b4d10df67d2a3ef4f7b3f5f181102e
Sajal Gupta <sajal07022004@gmail.com>
2025-08-05 13:16:33 +0530

    RTE & PostForm Creation

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
 import Button from "./Button";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Input, Button}
+export { Header, Footer, Container, Logo, LogoutBtn, Input, Button, RTE }
